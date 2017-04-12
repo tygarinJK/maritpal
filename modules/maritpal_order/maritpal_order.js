fce53ed2e6aec6d9465a661b9e3f7d4df3269d2f
Eugene <Eugene>
2017-04-12 09:39:17 +0300

    Initial

---
+++ modules/maritpal_order/maritpal_order.js
@@ -0,0 +1,33 @@
+;(function($) {
+
+	Drupal.behaviors.maritpal_order = {
+		'attach': function(context, settings) {
+
+			$(document).ready(function($) {
+
+				// console.log(Drupal.settings.basePath);
+
+				// var UserLocation = function (){
+				// 	return this;
+				// };
+
+
+				// $.getJSON('/rest/vocabulary/14', {}, function(json, textStatus) {
+				// 	console.log(json);
+				// 	console.log(textStatus);
+				// });
+
+
+
+
+
+				
+			});
+
+
+
+
+		}
+	}
+
+}) (jQuery);
